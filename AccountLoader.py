eda561a483189d8dca07d562dc69a1c2c93cf474
bbielsa <bgb7@njit.edu>
2016-04-17 15:19:53 -0400

    Adding account configuration

--- AccountLoader.py
+++ AccountLoader.py
@@ -2,6 +2,11 @@ import json
 
 
 class AccountLoader:
-    def __init__(self, accounts_file):
+    def __init__(self, accounts_file_path):
+        self.accounts = []
+        self.accounts_file = open(accounts_file_path, "r")
 
+    def load_accounts(self):
+
+        json.load()
 
